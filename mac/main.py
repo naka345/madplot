1761dcd641ed911c7e04884a12e9008da4d43075
naka345 <nakaka33@yahoo.co.jp>
2020-08-06 23:41:19 +0900

    pyoxidizer run

---
+++ mac/main.py
@@ -0,0 +1,61 @@
+import csv
+import os
+import sys
+import glob
+import fire
+from script.plot import Plot
+from script.read_config import ReadConfig
+
+
+class Main:
+    def __init__(self, path="./config/config_template.yml"):
+        self.path = path
+
+    def read_yaml_config(self):
+        config = ReadConfig(self.path)
+        read_yaml = config.yaml_config_read()
+        self.read_config_dict, self.rcParams_dict = ReadConfig.separate_rcParams(
+            read_yaml
+        )
+
+    def init_plot_configure(self):
+        self.plt_class = Plot(self.read_config_dict, self.rcParams_dict)
+        self.plt_class.init_figure()
+
+    def set_data_files(self):
+        file_list = self.read_config_dict["datafile"]["files"].split(",")
+        file_dir = self.read_config_dict["datafile"]["filedir"]
+
+        data_files_dict = {}
+        for fl in file_list:
+            if "*" in fl:
+                all_file_list = glob.glob(f"{file_dir}/{fl}")
+                all_file_dict = {file.split("/")[-1]: file for file in all_file_list}
+                data_files_dict = {**data_files_dict, **all_file_dict}
+            else:
+                data_files_dict.update({fl: f"{file_dir}/{fl}"})
+        return data_files_dict
+
+
+def madplot(config_path="", std_err=True):
+    main = Main(path=config_path) if config_path else Main()
+    main.read_yaml_config()
+    main.init_plot_configure()
+
+    files_dict = main.set_data_files()
+    df_dict = {k.split(".")[0]: Plot.read_csv(v) for k, v in files_dict.items()}
+
+    std_err = Plot.std_err_df(df_dict) if std_err else None
+
+    for csv_name, df in df_dict.items():
+        subplot_ax = main.plt_class.main_df_plot(df.T, std_err_df=std_err)
+        main.plt_class.set_axes_config(title=csv_name)
+        main.plt_class.set_label_name()
+
+        # main.plt_class.figure_show()
+        main.plt_class.figure_save()
+        main.plt_class.remove_axes(subplot_ax)
+
+
+if __name__ == "__main__":
+    fire.Fire({"madplot": madplot})
---
+++ mac/script/plot.py
@@ -0,0 +1,111 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+import pandas as pd
+import math
+
+
+class Plot:
+    def __init__(self, read_yaml, rcParams_dict):
+        self.figure_config = read_yaml
+        self.rcParams = rcParams_dict
+        self.file_count = 0
+
+    @staticmethod
+    def read_csv(path):
+        return pd.read_csv(path, header=0, index_col=0)
+
+    @staticmethod
+    def std_err_df(df_dict):
+        df_list = [df for df in df_dict.values()]
+        append_dict = {index: {} for index, df in df_list[0].iterrows()}
+
+        for i, df in enumerate(df_list):
+            for index, series in df.iterrows():
+                append_dict[index].update({str(i): series})
+
+        key_concat = {
+            index: pd.DataFrame(index_series_list).T
+            for index, index_series_list in append_dict.items()
+        }
+        std_err_dict = {
+            key: (concat_df.std(ddof=False) / math.sqrt(len(concat_df)))
+            for key, concat_df in key_concat.items()
+        }
+        std_err_df = pd.DataFrame(std_err_dict)
+        return std_err_df
+
+    def init_figure(self):
+        plt.style.use(self.rcParams)
+        plt_option = self.figure_config["figure"]
+        self.fig = plt.figure(**plt_option)
+
+    def get_line_option(self):
+        return self.figure_config["lines"]
+
+    def get_legend_option(self):
+        return self.figure_config["legend"]
+
+    def get_errbar_option(self, std_err_df):
+        errbar_config = {"yerr": std_err_df}
+        if "errbar" in self.figure_config:
+            errbar_config = {**errbar_config, **self.figure_config["errbar"]}
+        return errbar_config
+
+    def set_axes_config(self, **kargs):
+        axes_config = self.figure_config["axes"]
+        if "title" in kargs and axes_config["title"]["specific"] is False:
+            self.csv_title = kargs["title"]
+            self.ax.set_title(self.csv_title)
+        else:
+            self.ax.set_title(axes_config["title"]["name"])
+        self.ax.set_xscale(axes_config["scale"]["xscale"])
+        self.ax.set_yscale(axes_config["scale"]["yscale"])
+        log_format = eval(f'ticker.{axes_config["scale"]["ticker"]}')()
+        self.ax.get_yaxis().set_major_formatter(log_format)
+
+    def set_label_name(self):
+        axis_config = self.figure_config["axis"]
+        self.ax.set_xlabel(axis_config["xlabel"])
+        self.ax.set_ylabel(axis_config["ylabel"])
+
+    def main_df_plot(self, csv_df, std_err_df=None):
+        self.ax = self.fig.add_subplot(1, 1, 1)
+
+        errbar_option = (
+            self.get_errbar_option(std_err_df) if std_err_df is not None else {}
+        )
+        legend_option = self.get_legend_option()
+        get_line_option = self.get_line_option()
+
+        karg_option = {**errbar_option, **legend_option, **get_line_option}
+        ax_subplot = csv_df.plot(ax=self.ax, **karg_option)
+
+        return ax_subplot
+
+    def remove_axes(self, ax):
+        self.fig.delaxes(ax)
+
+    def figure_save(self, path=None):
+        output_dir = self.figure_config["output"]["dir"]
+        ext = self.figure_config["output"]["extension"]
+
+        if path is not None:
+            self.fig.savefig(path)
+        elif self.figure_config["output"]["title"]["specific"] is False and hasattr(
+            self, "csv_title"
+        ):
+            file_path = f"{output_dir}/{self.csv_title}.{ext}"
+            self.fig.savefig(file_path)
+        else:
+            file_name = self.figure_config["output"]["title"]["filename"]
+            if self.file_count == 0:
+                file_path = f"{output_dir}/{file_name}.{ext}"
+            else:
+                file_path = f"{output_dir}/{file_name}_{self.file_count}.{ext}"
+            self.fig.savefig(file_path)
+            self.file_count += 1
+
+    def figure_show(self):
+        self.fig.show()
