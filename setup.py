a956edace6aaebfdbd7df0ed659170bd8a22c418
naka345 <nakaka33@yahoo.co.jp>
2020-08-24 16:02:11 +0900

    add setup files
    deploy test
    
    dep test
    
    tmp

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+import configparser
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+config = configparser.ConfigParser()
+pip_file = config.read("Pipfile")
+packages = [package for package in config["packages"]]
+
+setuptools.setup(
+    name="madplot",
+    version="0.1.1",
+    install_requires=packages,
+    entry_points={
+        'console_scripts': [
+            'madplot=madplot:main',
+        ],
+    },
+    author="naka345",
+    author_email="nakaka33@yahoo.co.jp",
+    description="package test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/naka345/madplot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    python_requires='>=3.8',
+)
